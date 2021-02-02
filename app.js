94523c3cba63da7a24215a5489c2ea12b1b6fc73
iamniama <plectrumonthespectrum@gmail.com>
2021-02-02 16:23:40 -0700

    replicated dino functionality for cryptid data

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get("/dinosaurs/:idx", (req, res)=> {
 
 app.get("/dinosaurs/edit/:idx", (req, res)=>{
     let dinos = getFile("./dinosaurs.json")
-    res.render("dinosaurs/edit", {title: "dinosaurs", dino: dinos[req.param.idx], dinoId: req.params.idx}
+    res.render("dinos/edit", {title: "dinosaurs", dino: dinos[req.params.idx], dinoId: req.params.idx})
 })
 
 app.post("/dinosaurs", (req, res)=>{
@@ -55,5 +55,56 @@ app.delete("/dinosaurs/:idx", (req, res)=>{
     res.redirect("/dinosaurs")
 })
 
+app.put("/dinosaurs/:idx", (req, res)=>{
+    let dinos = getFile("./dinosaurs.json")
+    dinos[req.params.idx] = {name: req.body.name, type: req.body.type}
+    writeFile("./dinosaurs.json", dinos)
+    res.redirect("/dinosaurs")
+})
+
+
+////////////////////////////////////////////////////////
+
+app.get("/cryptids", (req, res)=>{
+    let cryptids = getFile("./cryptids.json")
+    res.render('cryptids/index', {title: "cryptids", critters: cryptids })
+})
+
+app.get("/cryptids/new", (req, res)=>{
+    res.render("cryptids/new", {title: "cryptids"})
+})
+
+app.get("/dinosaurs/:idx", (req, res)=> {
+    let cryptids = getFile("./cryptids.json")
+    res.render("cryptids/index", {title: "cryptids", critters: [cryptids[req.params.idx]] })
+})
+
+app.post("/cryptids", (req, res)=>{
+    let cryptids = getFile("./cryptids.json")
+    cryptids.push(req.body)
+    writeFile("./cryptids.json", cryptids)
+    res.redirect("/cryptids")
+})
+
+app.get("/cryptids/edit/:idx", (req, res)=>{
+    let cryptids = getFile("./cryptids.json")
+    res.render("cryptids/edit", {title: "cryptids", critter: cryptids[req.params.idx], critterId: req.params.idx})
+})
+
+
+
+app.delete("/cryptids/:idx", (req, res)=>{
+    let cryptids = getFile("./cryptids.json")
+    cryptids.splice(req.params.idx, 1)
+    writeFile("./cryptids.json", cryptids)
+    res.redirect("/cryptids")
+})
+
+app.put("/cryptids/:idx", (req, res)=>{
+    let cryptids = getFile("./cryptids.json")
+    cryptids[req.params.idx] = {name: req.body.name, img_url: req.body.img_url}
+    writeFile("./cryptids.json", cryptids)
+    res.redirect("/cryptids")
+})
 
 app.listen(port)
